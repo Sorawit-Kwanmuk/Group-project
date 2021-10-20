10c663f50b41acb1b66bed01faf6674aee07fe14
Sorawit_Kwanmuk <sorawit_kwanmuk@outlook.com>
2021-10-20 10:34:05 +0700

    แก้ไข ต้อง clone develop แล้วไปทำใน branch feature

--- group-project-front-end/src/App.js
+++ group-project-front-end/src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
-import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import Login from './components/Login/Login';
-import Register from './components/Register/Register';
 
 function App() {
-  return (
-    <>
-      {/* <Login /> */}
-      <Register />
-      {/* <Header />
-      <Home />
-      <Footer /> */}
-    </>
-  );
+  return <></>;
 }
 
 export default App;
